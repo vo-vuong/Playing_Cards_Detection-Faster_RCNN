1ec46b3a6258b1c362da3574c5aa83c1ec05d227
vovuong <vovuong1025@gmail.com>
2025-10-09 09:52:16 +0700

    refactor: improve annotation handling and coordinate normalization in PlayingCardDataset

--- dataset.py
+++ dataset.py
@@ -1,20 +1,21 @@
-import os
-from torch.utils.data import Dataset
-from PIL import Image
 import json
-import torch
+import os
+from collections import defaultdict
+
 import cv2
 import numpy as np
-from constants.paths_const import DATA_PATH, ANNOTATIONS_ORG_FILE
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+
 from constants.config_const import CLASSES
+from constants.paths_const import ANNOTATIONS_ORG_FILE, DATA_PATH
 
 
 class PlayingCardDataset(Dataset):
     def __init__(self, root=DATA_PATH, train=True, transform=None, size=640):
-        self.images = []
-        self.annotations = []
-        self.size = size
         self.transform = transform
+        self.size = size
         self.classes = CLASSES
 
         self.data_path = root
@@ -25,10 +26,19 @@ class PlayingCardDataset(Dataset):
 
         # Read annotations file
         annotations_file = os.path.join(self.data_path, ANNOTATIONS_ORG_FILE)
-        with open(annotations_file, "r") as file:
+        with open(annotations_file, "r", encoding="utf-8") as file:
             data_annotation = json.load(file)
+
         self.images = data_annotation["images"]
-        self.annotations = data_annotation["annotations"]
+        self.image_to_annotations = self._build_annotation_index(
+            data_annotation["annotations"]
+        )
+
+    def _build_annotation_index(self, annotations):
+        index = defaultdict(list)
+        for ann in annotations:
+            index[ann["image_id"]].append(ann)
+        return index
 
     def __len__(self):
         return len(self.images)
@@ -39,38 +49,52 @@ class PlayingCardDataset(Dataset):
         image = Image.open(image_path).convert("RGB")
 
         # get annotations of this item
-        annotations = []
-        for ann in self.annotations:
-            if ann["image_id"] == self.images[idx]["id"]:
-                annotations.append(ann)
+        annotations = self.image_to_annotations.get(current_image["id"], [])
 
         boxes = []
         labels = []
-        ori_width = int(current_image["width"])
-        ori_height = int(current_image["height"])
+
         for annotation in annotations:
             bbox = annotation["bbox"]
-            x_min = int(bbox[0]) / ori_width * self.size
-            y_min = int(bbox[1]) / ori_height * self.size
-            x_max = int(bbox[2] + bbox[0]) / ori_width * self.size
-            y_max = int(bbox[3] + bbox[1]) / ori_height * self.size
-            boxes.append([x_min, y_min, x_max, y_max])
-            labels.append(annotation["category_id"])
-        boxes = torch.FloatTensor(boxes)
-        labels = torch.LongTensor(labels)
-        targets = {"boxes": boxes, "labels": labels}
+            # Normalize coordinates to target size
+            x_min = bbox[0]
+            y_min = bbox[1]
+            x_max = bbox[0] + bbox[2]
+            y_max = bbox[1] + bbox[3]
+
+            if x_max > x_min and y_max > y_min:
+                boxes.append([x_min, y_min, x_max, y_max])
+                labels.append(annotation["category_id"])
+
+        if len(boxes) == 0:
+            boxes = torch.zeros((0, 4), dtype=torch.float32)
+            labels = torch.zeros((0,), dtype=torch.int64)
+        else:
+            boxes = torch.as_tensor(boxes, dtype=torch.float32)
+            labels = torch.as_tensor(labels, dtype=torch.int64)
+
+        target = {
+            "boxes": boxes,
+            "labels": labels,
+        }
 
         if self.transform:
-            image = self.transform(image)
+            image, target = self.transform(image, target)
 
-        return image, targets
+        return image, target
 
 
 if __name__ == "__main__":
     train_set = PlayingCardDataset(train=False)
 
-    image, label = train_set.__getitem__(1)
-    cv2_image = np.array(image)
+    img, targets = train_set[1]
+    cv2_image = np.array(img)
     cv2_image = cv2.cvtColor(cv2_image, cv2.COLOR_RGB2BGR)
+
+    # Draw bounding boxes for visualization
+    for box in targets["boxes"]:
+        x1, y1, x2, y2 = map(int, box)
+        cv2.rectangle(cv2_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
     cv2.imshow("image", cv2_image)
     cv2.waitKey(0)
